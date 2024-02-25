275f0b0a39e72dc84c60b96c67bb14427a0fabb4
Sardar Tariq Aziz <69082943+9874sardar@users.noreply.github.com>
2024-02-26 01:40:35 +0530

    Task files

---
+++ App.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Gallery from './screens/Gallery';
+import Home from './screens/Home';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="gallery" component={Gallery} />
+        <Stack.Screen name="home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
